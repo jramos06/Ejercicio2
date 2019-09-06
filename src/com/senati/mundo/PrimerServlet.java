c8a3b734f53ef1d8416f7b4331ecace511f3d33c
alumno <alumno@10.0.2.15>
2019-09-06 23:20:25 +0200

    Ejercicio2

---
+++ src/com/senati/mundo/PrimerServlet.java
@@ -0,0 +1,53 @@
+package com.senati.mundo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimerServlet
+ */
+public class PrimerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrimerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = new PrintWriter(response.getOutputStream());
+		out.println("<html>");
+		out.println("<head><title>Hola mundo Servlet </title></head>");
+		out.println("<body>");
+		out.println("<h1><center>Hola mundo desde el Servidor web </center></h1>");
+		out.println("<a href='mundo.html'>Volver</a>");
+		out.println("</body></html>");
+		
+		out.close();
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+	}
+
+}
